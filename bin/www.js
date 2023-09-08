f616bfeadc5e5fbc4897ec8f6b7909c2ad8c7bf1
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-08 12:59:35 -0300

    multer configurado

--- bin/www.js
+++ bin/www.js
@@ -1,46 +1,55 @@
-import app from '../app.js'               //importo la aplicación de back configurada
-import debug from 'debug'                 //importo el modulo para debuguear
-import http from 'http'                   //importo el módulo para crear servidores
 
-let port = normalizePort(process.env.PORT || '8084')      //defino el puerto donde va a funcionar nuestro servidor
-//defino un operador OR para normalizar el puerto
-//porque cuando hostee/deployee el servidor el hosting me va a asignar el puerto que tenga libre para mi app
+import app from '../app.js' ;
+import debug from 'debug';
+import http from 'http' ;
+import bodyParser from 'body-parser';
+const logger= debug('levantando-new-server')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+let port = normalizePort(process.env.PORT || '8084');
+app.set('port', port);
 
-app.set('port', port)                     //configuro el puerto con la variable port definida anteriormente
-
-let server = http.createServer(app)       //utilizo el módulo http para crear un servidor (con las configuraciones que se realizaron en app)
-
-function ready() {                        //ready es una función que se va a ejecutar cuando se levante correctamente el servidor
-    console.log('server ready on port '+port)           //me informa en la consola que el servidor se levantó correctamente en el puerto PORT
+let server = http.createServer(app);
+function ready(){
+  console.log('Your server it´s ready on port'+port);
 }
-//listen se utiliza para escuchar un puerto y es el encargado de levantar efectivamente el servidor (empieza a funcionar)
 server.listen(
-    port,                                 //primer parámetro necesario el puerto donde tiene que funcionar el servidor
-    ready                                 //segundo parámetro callback opcional para mostrar en consola que el servidor está funcionando
-)
+  port,
+  ready,
+  );
 server.on('error', onError);
 server.on('listening', onListening);
 
 function normalizePort(val) {
   let port = parseInt(val, 10);
+
   if (isNaN(port)) {
     // named pipe
     return val;
   }
+
   if (port >= 0) {
     // port number
     return port;
   }
+
   return false;
 }
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
+
   let bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
@@ -54,6 +63,10 @@ function onError(error) {
       throw error;
   }
 }
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
 function onListening() {
   let addr = server.address();
--- controllers/clientes/create.js
+++ controllers/clientes/create.js
@@ -1,20 +1,52 @@
-import Clientes from "../../models/Clientes";
+import Clientes from "../../models/Clientes.js";
+import Estados from "../../models/Estados.js"; // Asegúrate de importar tu modelo de Estados
 
-export default async(req, res)=>{
-try {
-    let create_clientes=Clientes.create(req.body)
-    if(create_clientes){
-    res.status(201).json({
-    response:create_clientes,
-    message:"Cliente creado"
-    })
-    }else{
-    res.status(400).json({
-    response:null,
-    message:"ocurrio un error al crear el cliente"
-    })
+export default async (req, res, next) => {
+  try {
+    const photoUrl = req.file ? req.file.path : null;
+    let clienteData = { ...req.body, foto: photoUrl };
+
+    // Verificar si se proporcionó el nombre del estado
+    if (clienteData.estado_id) {
+      const estado = await Estados.findOne({ nombre: clienteData.estado_id });
+
+      if (!estado) {
+        return res.status(400).json({
+          response: null,
+          message: `El estado "${clienteData.estado_id}" no existe en la base de datos.`,
+        });
+      }
+
+      // Asignar el ObjectId del estado a la propiedad estado_id
+      clienteData = {
+        ...clienteData,
+        estado_id: estado._id,
+      };
     }
-} catch (error) {
-console.log(error);
-}
-}
+
+    const all = await Clientes.create(clienteData);
+
+    if (all) {
+      const imageUrl = photoUrl ? photoUrl.replace("\\", "/") : null;
+      const response = imageUrl
+        ? { ...all._doc, foto: imageUrl }
+        : all._doc;
+
+      return res.status(201).json({
+        response: response,
+        message: "Cliente creado exitosamente.",
+      });
+    } else {
+      return res.status(400).json({
+        response: null,
+        message: "Ocurrió un error al crear el cliente.",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      response: null,
+      message: "Ocurrió un error interno en el servidor.",
+    });
+  }
+};
