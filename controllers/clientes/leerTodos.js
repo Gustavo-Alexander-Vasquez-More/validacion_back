d52756f272ac21a531f82c0f1a0afcf22fc39bef
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-26 18:54:28 -0300

    populate

--- controllers/clientes/leerTodos.js
+++ controllers/clientes/leerTodos.js
@@ -3,6 +3,14 @@ import Clientes from "../../models/Clientes.js";
 export default async (req, res)=>{
 try {
     let all =await Clientes.find(req.body)
+    .populate({
+        path: 'estado_id',
+        select: 'nombre'
+      })
+      .populate({
+        path: 'author_id',
+        select: 'usuario'
+      })
 if(all){
     res.status(200).json({
         response:all,
