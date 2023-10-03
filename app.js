e1c6001625eaf952458d4cf49c8c6f74eb67b6b8
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-10-03 11:44:12 -0300

    other ws

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
-import 'dotenv/config.js'//configura las variables de entorno de la aplicacion
-import "./config/db.js"
+// Importa las librerías necesarias
+import 'dotenv/config.js';
+import './config/db.js';
 import { __dirname } from './utils.js';
-import cors from 'cors'//modulo para permitir origenes cruzados(puerto del back con puerto del front)
-import express from'express'; //modulo necesario para levantar y configurar un servidor
-import indexRouter from'./routes/index.js';//enrutador principal de la aplicacion
-import logger from'morgan';//modulo para registrar las peticiones que se reaslizan al servidor
-import path from'path'; //modulo necesario para conocer la ubicaciob de nuestro servidor
+import cors from 'cors';
+import express from 'express';
+import expressWs from 'express-ws';
+import indexRouter from './routes/index.js';
+import logger from 'morgan';
+import path from 'path';
 import bodyParser from 'body-parser';
 
-let app = express(); 
-app.disable('etag')
+const app = express();
+const { app: appWithWs, getWss } = expressWs(app);
+
+app.disable('etag');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors()) //obliga al servidor a cruzar los origrenes del front y back
-app.use(logger('dev')); //obliga al servidor a usar el middleware de registro de peticiones
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')));//obliga al servidor a generar una carpeta de acceso PUBLICO
-app.set('views', path.join(__dirname, 'views'));//configuro que las vistas generadas en el backend estan en la capeta views
-app.set('view engine', 'ejs');//configuro que las listas se van a definir con el lenguaje de EJS.(moto de plantilla)
-app.use('/api', indexRouter);//obliga al servidor a usar las rutas definifas en el enrutador
-app.use('/uploads',express.static('uploads'))
-app.use((req, res, next)=>{
-console.log('Time: ', new Date().getFullYear()); //Imprimir 2023
-next()});
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/api', indexRouter);
+app.use('/uploads', express.static('uploads'));
+app.use((req, res, next) => {
+  console.log('Time: ', new Date().getFullYear());
+  next();
+});
 
+// Configura una ruta para las conexiones WebSocket
+appWithWs.ws('/socket', (ws, req) => {
+  console.log('Usuario conectado al WebSocket');
 
+  // Escucha mensajes desde el cliente
+  ws.on('message', (message) => {
+    console.log('Mensaje recibido:', message);
+
+    // Puedes enviar mensajes de vuelta al cliente
+    ws.send('Mensaje recibido por el servidor: ' + message);
+  });
+
+  // Maneja la desconexión del cliente
+  ws.on('close', () => {
+    console.log('Usuario desconectado del WebSocket');
+  });
+});
 
 export default app;
