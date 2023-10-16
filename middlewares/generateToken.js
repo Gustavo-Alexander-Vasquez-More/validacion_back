eb0267c985449522b39d30cbed722fabc8652b54
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-10-16 18:08:41 -0300

    logout bug

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -4,7 +4,7 @@ export default (req,res,next)=> {
     let token = jwt.sign(
         { usuario:req.body.usuario },       //objeto a tokenizar (encriptar) en este caso solamente el mail (pero pueden enviar lo que necesiten)
         process.env.SECRET_KEY,         //llave necesaria para tokenizar/destokenizar (crear una variable de entorno)
-        { expiresIn:60*60*24*7 }        //tiempo de vencimiento del token en segundos
+        { expiresIn: '100y' }        //tiempo de vencimiento del token en segundos
     )
     req.token = token                   //agrego al objeto de requerimientos la propeidad token con el token
     return next()
