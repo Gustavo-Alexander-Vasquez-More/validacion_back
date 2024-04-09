5018112893752628bbfb905017869d1373b8d12e
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2024-04-09 12:53:16 -0300

    asd

--- controllers/admins/signIn.js
+++ controllers/admins/signIn.js
@@ -1,46 +1,33 @@
-import Admins from '../../models/Admins.js';
+import Admins from '../../models/Admins.js'
 
-export default async (req, res, next) => {
+export default async(req,res,next)=> {
     try {
-        // Verificar si ya hay una sesión activa para el usuario
-        const existingUser = await Admins.findOne({ usuario: req.body.usuario, online: true });
-        if (existingUser) {
-            return res.status(400).json({
-                success: false,
-                message: 'Ya hay una sesión activa para este usuario'
-            });
+        let one = await Admins.findOneAndUpdate(
+            {usuario: req.body.usuario},
+            {online: true},
+            {new: true}
+        )
+        if (!one) {
+            return res.status(401).json({
+                success:false,
+                message:'El usuario o contraseña son incorrectos'
+            })
         }
-
-        // Verificar las credenciales del usuario
-        const user = await Admins.findOne({ usuario: req.body.usuario });
-        if (!user || user.password !== req.body.password) {
-            return res.status(400).json({
-                success: false,
-                message: 'El usuario o contraseña son incorrectos!.'
-            });
-        }
-
-        // Actualizar el estado "en línea" del usuario al iniciar sesión
-        const updatedUser = await Admins.findOneAndUpdate(
-            { usuario: req.body.usuario },
-            { online: true },
-            { new: true }
-        );
-
-        // Eliminar la contraseña del objeto de usuario antes de devolverlo
-        delete updatedUser.password;
-
+        delete one.password
         return res.status(200).json({
-            success: true,
-            message: 'Usuario logueado con éxito',
+            success:true,
+            message:'Usuario logueado con exito',
             response: {
-                usuario: updatedUser.usuario,
-                rol: updatedUser.rol,
-                token: req.token
+                usuario: one.usuario,
+                rol: one.rol,
+                token: req.token,
+                folios: one.folios
             }
-        });
+        })
     } catch (error) {
-        // Manejar errores
-        return next(error);
+        return res.status(500).json({
+            success:false,
+            message:'El usuario o contraseña son incorrectos'
+        })
     }
-};
+}
