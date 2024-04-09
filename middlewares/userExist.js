490635f9a3bf218e849ed932e8b2f0da4d9bbc7d
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2024-04-09 13:12:44 -0300

    sad

---
+++ middlewares/userExist.js
@@ -0,0 +1,25 @@
+import Admins from "../models/Admins.js"
+
+export default async function(req,res,next) {
+
+    try{
+        const usuarios = await Admins.findOne({ usuario: req.body.usuario })
+        if (usuarios) {
+            return res.status(400).json({
+                success: false,
+                message: 'Bienvenido'
+            })
+        }
+        if (!usuarios) {
+            return res.status(400).json({
+                success: false,
+                message: 'El usuario o contraseña son incorrectos!'
+            })
+        }
+        return next()
+    }
+    catch(error){
+
+        return next(error)
+    }
+}
