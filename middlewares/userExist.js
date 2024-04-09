47f28356a68bedf232eefa32377f4ba154d59be7
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2024-04-09 13:15:30 -0300

    dsa

--- middlewares/userExist.js
+++ middlewares/userExist.js
@@ -4,12 +4,7 @@ export default async function(req,res,next) {
 
     try{
         const usuarios = await Admins.findOne({ usuario: req.body.usuario })
-        if (usuarios) {
-            return res.status(400).json({
-                success: false,
-                
-            })
-        }
+       
         if (!usuarios) {
             return res.status(400).json({
                 success: false,
